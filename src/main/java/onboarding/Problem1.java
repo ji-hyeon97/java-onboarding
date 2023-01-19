989da5e1bc5e514b660480cc5d2387ab5ae1bc26
SeoJiHyeon <sgh9702@naver.com>
2023-01-19 16:20:28 +0900

    [feat] : 페이지 번호 각 자리수 더해서 가장 큰 수 구현 & 각자리수 곱해서 가장 큰수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,47 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final int pobiWin = 1;
+    private static final int crongWin = 2;
+    private static final int draw = 0;
+    private static final int exception = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        return 0;
+    }
+
+    public static int sum(List<Integer> pageNumbers){
+        int maxSumNumber = 0;
+        for(int i=0; i< pageNumbers.size(); i++){
+            int pageNumber = pageNumbers.get(i);
+            int sum = 0;
+            while(true){
+                if (pageNumber<=0){
+                    break;
+                }
+                sum += pageNumber%10;
+                pageNumber = (int)(pageNumber/10);
+            }
+            maxSumNumber = sum;
+        }
+        return maxSumNumber;
+    }
+
+    public static int multiple(List<Integer> pageNumbers){
+        int maxMultiplyNumber = 0;
+        for(int i=0; i< pageNumbers.size(); i++){
+            int pageNumber = pageNumbers.get(i);
+            int sum = 1;
+            while(true){
+                if (pageNumber<=0){
+                    break;
+                }
+                sum *= pageNumber%10;
+                pageNumber = (int)(pageNumber/10);
+            }
+            maxMultiplyNumber = sum;
+        }
+        return maxMultiplyNumber;
     }
 }
